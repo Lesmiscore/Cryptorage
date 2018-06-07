670674bae80f7e741c1ae7e0eba9744d56e6a6d4
nao20010128nao <nao20010128@gmail.com>
2018-06-07 21:54:57 +0900

    短縮

--- src/main/kotlin/com/nao20010128nao/Cryptorage/cryptorage/ReadOnlyCryptorage.kt
+++ src/main/kotlin/com/nao20010128nao/Cryptorage/cryptorage/ReadOnlyCryptorage.kt
@@ -1,20 +1,11 @@
 package com.nao20010128nao.Cryptorage.cryptorage
 
 import com.google.common.io.ByteSink
-import com.google.common.io.ByteSource
 import com.nao20010128nao.Cryptorage.Cryptorage
 import com.nao20010128nao.Cryptorage.internal.readOnly
 
-internal class ReadOnlyCryptorage(private val base: Cryptorage) : Cryptorage {
+internal class ReadOnlyCryptorage(private val base: Cryptorage) : Cryptorage by base {
     override val isReadOnly: Boolean = true
-
-    override fun list(): Array<String> = base.list()
-    override fun open(name: String, offset: Int): ByteSource = base.open(name, offset)
-    override fun lastModified(name: String): Long = base.lastModified(name)
-    override fun size(name: String): Long = base.size(name)
-    override fun meta(key: String): String? = base.meta(key)
-    override fun close() = base.close()
-
     override fun put(name: String): ByteSink = readOnly("Cryptorage")
     override fun mv(from: String, to: String) = readOnly("Cryptorage")
     override fun delete(name: String) = readOnly("Cryptorage")
